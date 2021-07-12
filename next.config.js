f73efcb02e68a0ef8f1d413e2efb61d4bec8e6ca
4very <asommer999@gmail.com>
2021-07-12 17:28:34 -0400

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  env: {
+    twitchUsername: process.env.twitchUsername,
+    twitchClientID: process.env.twitchClientID,
+    twitchPrivateKey: process.env.twitchPrivateKey,
+    WCLClientID: process.env.WCLClientID,
+    WCLClientPrivate: process.env.WCLClientPrivate,
+  },
+};
